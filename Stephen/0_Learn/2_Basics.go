745844b3cebd3072654fe95640c2edada5201fb3
Gaurav Gulati <83610860+Gulati-Gaurav@users.noreply.github.com>
2024-11-24 12:59:18 +0530

    cc

--- Stephen/0_Learn/2_Basics.go
+++ Stephen/0_Learn/2_Basics.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func arrays_slices_for_discard_subset() {
+func arrays_slices() {
 	// array = array
 	// slice = vector
 	// you mention size it is array. You don't it is slice.
@@ -19,14 +19,19 @@ func arrays_slices_for_discard_subset() {
 
 	var arrayName [5]int                  // array
 	numbers := [5]int{10, 20, 30, 40, 50} // array
+}
 
+func for_discard_subset() {
+	cards2 := [5]int{1, 3, 4, 5, 8}
+	
 	// iterate. i = index, card = element at ith index.
 	// range is keyword for every element in slice. first is index, second is element.
-	for i, card := range cards {
-		fmt.Println(i, card, cards[i])
+	for i, card := range cards2 {
+		fmt.Println(i, card, cards2[i])
 	}
+
 	// for every iteration i, card are initialised i.e. card is not actual cards[i] but rather a copy of cards[i] created using card:=cards[i]. Proof:-
-	cards2 := [5]int{1, 3, 4, 5, 8}
+
 	for i, card := range cards2 {
 		card = 12
 		fmt.Println(i, card, cards2[i])
@@ -45,13 +50,13 @@ func arrays_slices_for_discard_subset() {
 	// }
 
 	// the underscore (_) is a special identifier used to discard values. It's often used when you need to call a function or method that returns a value, but you don't need that value.
-	for _, card := range cards {
+	for _, card := range cards2 {
 		fmt.Println(card)
 	}
 
 	// To get subset. [startIndexIncluding : upToNotIncluding]. [:n] means start from 0. [1:] means go to end
 	// will not modify the original slice. returns a new slice
-	subCard := cards[0:2]
+	subCard := cards2[0:2]
 
-	fmt.Println(cards, subCard, arrayName, numbers)
+	fmt.Println(cards2, subCard, arrayName, numbers)
 }
